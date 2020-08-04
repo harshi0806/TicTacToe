81abd241c32f2e34d81d1c33acdf441d3eec0b80
harshi0806 <harshitashrivatav121@gmail.com>
2020-08-04 19:28:17 +0530

    Refactored:UC-1 Logical Implication

--- TicTacToe.java
+++ TicTacToe.java
@@ -18,59 +18,5 @@ public class TicTacToe {
 	static void populateEmptyBoard() {
 		for (int a = 0; a < 9; a++) {
 			board[a] = String.valueOf(a+1);
-		}
-		currentState = PLAYING;
-		currentPlayer = Toss();
-      if(currentPlayer == 1)
-      {
-         currentPlayer = CROSS;
-      }
-      else
-      {
-         currentPlayer = NOUGHT;
-      }
-	}
-
-	//Function to generate random player
-	public static int Toss() {
-		final Random r = new Random();
-		return r.nextInt(1) + 1;
-	}
-
-	//Function to print the game board
-	public static void printBoard() {
-		for(int row = 0; row < ROWS; ++row)
-		{
-			for(int col = 0; col < COLS; ++col)
-			{
-				printCell(board[row][col]); //print each of the cells
-				if(col != COLS - 1)
-				{
-					System.out.println("|"); //print vertical partition
-				}
-			}
-			System.out.println();
-			if(row != ROWS - 1)
-			{
-				System.out.println("----------"); //print horizontal partition
-			}
-		}
-		System.out.println();
-	}
-
-	//Function to print a cell with the specified content
-	public static void printCell(int content) {
-		switch(content)
-		{
-			case EMPTY :
-				System.out.print(" ");
-				break;
-			case NOUGHT :
-				System.out.print(" O ");
-				break;
-			case CROSS :
-				System.out.print(" X ");
-				break;
-		}
 	}
 }
